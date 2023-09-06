b99b49e852cbd1759ffe12de0ef83754c71374b4
Rama Edwinda Putra <ramaedwindap@gmail.com>
2023-09-06 17:54:50 +0700

    fix: edit style PostTable component

--- client-admin/src/components/PostTable.jsx
+++ client-admin/src/components/PostTable.jsx
@@ -1,6 +1,8 @@
+import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
 import React from "react";
 
-export default function PostTable() {
+export default function PostTable({ posts }) {
+  // console.log(posts);
   return (
     <table className="w-full text-sm text-left text-gray-500">
       <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
@@ -15,33 +17,50 @@ export default function PostTable() {
             Content
           </th>
           <th scope="col" className="px-6 py-3 ">
-            <div className="w-[200px] text-center">Category</div>
+            Category
           </th>
           <th scope="col" className="px-6 py-3">
-            <div className="w-[200px] text-center">Category</div>
+            Author
           </th>
-          <th className="px-6 py-3 sr-only">Created By</th>
+          <th className="px-6 py-3 sr-only">Action</th>
         </tr>
       </thead>
       <tbody>
-        <tr className="bg-white border-b ">
-          <th
-            scope="row"
-            className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-          >
-            Apple MacBook Pro 17"
-          </th>
-          <td className="px-6 py-4">Silver</td>
-          <td className="px-6 py-4">Laptop</td>
-          <td className="px-6 py-4">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab
-            corporis harum ipsam eveniet quisquam doloremque quam eius
-            perferendis, tempora dignissimos tenetur asperiores officiis.
-            Placeat sit fugit maxime dolore rerum ab.
-          </td>
-          <td className="px-6 py-4">$2999</td>
-          <td className="px-6 py-4">$2999</td>
-        </tr>
+        {posts.map((post) => {
+          return (
+            <tr className="text-gray-800 bg-white border-b" key={post.id}>
+              <th scope="row" className="px-6 py-4 font-medium ">
+                <p className="max-w-[200px]">{post?.title}</p>
+              </th>
+              <td className="px-6 py-4">
+                <div className="w-[200px]">
+                  <img
+                    className="object-cover w-full rounded-lg h-28"
+                    src={post?.imgUrl}
+                    alt={post?.title}
+                  />
+                </div>
+              </td>
+              <td className="px-6 py-4">
+                <div className="w-[300px]">
+                  <p className="line-clamp-4">{post?.content}</p>
+                </div>
+              </td>
+              <td className="px-6 py-4">{post?.category?.name}</td>
+              <td className="px-6 py-4">{post?.author?.username}</td>
+              <td className="px-6 py-4">
+                <div className="flex items-center ">
+                  <button className="p-2 mr-2 border border-blue-800 rounded-md hover:text-blue-500 hover:border-blue-700 hover:bg-blue-50">
+                    <PencilSquareIcon className="w-5 h-5" />
+                  </button>
+                  <button className="p-2 mr-2 border border-red-500 rounded-md hover:text-red-500 hover:border-red-700 hover:bg-red-50">
+                    <TrashIcon className="w-5 h-5 " />
+                  </button>
+                </div>
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
