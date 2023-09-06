dfb25583b49d60c1baed178e579046aa03e2a1b7
Rama Edwinda Putra <ramaedwindap@gmail.com>
2023-09-06 17:53:33 +0700

    feat: create CategoryTable component

--- client-admin/src/views/Category.jsx
+++ client-admin/src/views/Category.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Title from "../components/Title";
 import { FolderOpenIcon } from "@heroicons/react/24/outline";
-import PostTable from "../components/PostTable";
+import CategoryTable from "../components/CategoryTable";
 
 export default function Category() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  // console.log(posts);
+
+  async function fetchCategories() {
+    try {
+      const response = await fetch("http://localhost:3000/categories", {
+        method: "GET",
+      });
+      const result = await response.json();
+      setCategories(result);
+      // console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div className="w-full p-4 bg-white border shadow-lg rounded-2xl">
+    <div className="w-full p-4 bg-white border shadow-lg rounded-2xl min-h-[500px]">
       <Title>
         <FolderOpenIcon />
         Categories
       </Title>
       <div className="relative mt-4 overflow-x-auto border rounded-lg ">
-        <PostTable />
+        <CategoryTable categories={categories} />
       </div>
     </div>
   );
