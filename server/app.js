3ae07f8aa8c4c1899eb21207ce6864fb0d8e7e6b
Rama Edwinda Putra <ramaedwindap@gmail.com>
2023-09-10 14:28:49 +0700

    fix: change config database for production

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require('express')
 const router = require('./routes')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const cors = require('cors')
 
 
@@ -18,10 +18,3 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
-// npx sequelize-cli model:generate --name User --attributes username:string,email:string,password:string,role:string,phoneNumber:string,address:string
-
-// npx sequelize-cli model:generate --name Category --attributes name:string
-
-// npx sequelize-cli model:generate --name Post --attributes title:string,slug:string,content:text,imgUrl:string,categoryId:integer,authorId:integer
-
-// npx sequelize-cli model:generate --name Tag --attributes postId:number,name:string
